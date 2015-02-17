56b806d9b221a16319fdf485841aa6f27d1d524b
Paul Deschner <deschner@comcast.net>
2015-02-17 02:42:59 -0500

    Style all comment blocks with pre tags to retain original formatting

--- db/schema.rb
+++ db/schema.rb
@@ -32,16 +32,6 @@ ActiveRecord::Schema.define(version: 20150212204520) do
     t.string   "converter_version", limit: 255
   end
 
-  create_table "conversions_bu", force: :cascade do |t|
-    t.text     "marc",            limit: 65535
-    t.text     "bf",              limit: 65535
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.string   "local_system_id", limit: 255
-    t.string   "title",           limit: 255
-    t.string   "evaluator_id",    limit: 255
-  end
-
   create_table "evaluators", force: :cascade do |t|
     t.string   "last_name",  limit: 255
     t.string   "first_name", limit: 255
